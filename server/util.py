0b829835d34493d3512fa6cfd99e01668bb9c5f2
chiraag-kakar <chirucos1998@gmail.com>
2020-10-23 00:36:20 +0530

    upload

---
+++ server/util.py
@@ -0,0 +1,53 @@
+import json
+import pickle
+
+import numpy as np
+
+__locations = None
+__data_columns = None
+__model = None
+
+
+def get_estimated_price(location, sqft, bhk, bath):
+    try:
+        loc_index = __data_columns.index(location.lower())
+    except:
+        loc_index = -1
+
+    x = np.zeros(len(__data_columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+    if loc_index >= 0:
+        x[loc_index] = 1
+
+    return round(__model.predict([x])[0], 2)
+
+
+def get_location_names():
+    return __locations
+
+
+def load_saved_artifacts():
+    print("Loading Saved Artifacts.....Start")
+    global __locations
+    global __data_columns
+
+    with open("./artifacts/columns.json", 'r') as f:
+        __data_columns = json.load(f)['data.columns']
+        __locations = __data_columns[3:]
+
+    global __model
+    with open("./artifacts/Real-Estate-Price-Prediction-Model.pickle", 'rb') as f:
+        __model = pickle.load(f)
+    print("Loading Saved Artifacts.....Done")
+
+
+if __name__ == '__main__':
+    load_saved_artifacts()
+    print(get_location_names())
+    print(get_estimated_price('1st Phase JP Nagar', 1000, 3, 3))
+    print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
+    print(get_estimated_price('Kalhalli', 1000, 2, 2))  # other location
+    print(get_estimated_price('Ejipura', 1000, 2, 2))  # other location
+
